187c2bbced235e4ad66ebbd7b35d4f2aa7edb9d2
d0yeong06 <mintdy09523@naver.com>
2024-07-29 01:15:12 +0900

    회원가입 로직 전체적 수정(현재 데이터베이스에 이름 입력 안 되는 오류 존재. 수정 요망)

--- auth.py
+++ auth.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, render_template, request, redirect, url_for, flash, session
-from models import db, add_student, add_teacher, Student, Teacher
+from models import db, add_student, add_teacher, add_admin, Student, Teacher, Admin
 import bcrypt
 
 auth_bp = Blueprint('auth', __name__)
@@ -19,14 +19,18 @@ def login():
         user = Student.query.filter_by(username=username).first()
     elif role == 'teacher':
         user = Teacher.query.filter_by(username=username).first()
+    elif role == 'admin':
+        user = Admin.query.filter_by(username=username).first()
 
     if user and bcrypt.checkpw(password.encode('utf-8'), user.password.encode('utf-8')):
         session['user_id'] = user.id
         session['role'] = role
         if role == 'student':
             return redirect(url_for('views.student_home'))
-        else:
+        elif role == 'teacher':
             return redirect(url_for('views.teacher_manage'))
+        elif role == 'admin':
+            return redirect(url_for('views.admin_home'))
     else:
         flash('Invalid credentials')
         return redirect(url_for('auth.index'))
@@ -42,14 +46,14 @@ def register():
         role = request.form['role']
         username = request.form['username']
         password = bcrypt.hashpw(request.form['password'].encode('utf-8'), bcrypt.gensalt()).decode('utf-8')
+        name = request.form['name']
 
         # Check if username already exists
-        if Student.query.filter_by(username=username).first() or Teacher.query.filter_by(username=username).first():
+        if Student.query.filter_by(username=username).first() or Teacher.query.filter_by(username=username).first() or Admin.query.filter_by(username=username).first():
             flash('The username already exists. Please use a different username.')
             return redirect(url_for('auth.register'))
 
         if role == 'student':
-            name = request.form['name']
             grade = request.form['grade']
             student_class = request.form['student_class']
             number = request.form['number']
@@ -64,15 +68,12 @@ def register():
             add_student(name, grade, student_class, number, username, password, barcode)
 
         elif role == 'teacher':
-            name = request.form['name']
             grade = request.form['grade']
             teacher_class = request.form['teacher_class']
             add_teacher(name, grade, teacher_class, username, password)
 
         elif role == 'admin':
-            name = request.form['name']
-            # 필요한 추가 필드 처리
-            add_teacher(name, "", "", username, password)  # 예시로, 추가 필드를 처리하지 않음
+            add_admin(name, username, password)
 
         return redirect(url_for('auth.index'))
 
